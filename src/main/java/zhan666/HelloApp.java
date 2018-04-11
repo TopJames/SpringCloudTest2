3a10b32e1a388cc507e1c0c9acd3cc0e6e9849a2
zhanlangqi <zhanlangqi@126.com>
2018-04-11 19:53:53 +0800

    asd

---
+++ src/main/java/zhan666/HelloApp.java
@@ -0,0 +1,12 @@
+package zhan666;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
